c91636f48df89940b84b994e31b5e9293fd74075
Samuel Caro <a20170086@pucp.edu.pe>
2020-09-15 09:21:30 -0500

    s

--- src/com/company/Docente.java
+++ src/com/company/Docente.java
@@ -2,4 +2,22 @@ package com.company;
 
 public class Docente extends Usuario{
 
+    private int añoInicio;
+    private int cantidadCursos;
+
+    public int getAñoInicio() {
+        return añoInicio;
+    }
+
+    public void setAñoInicio(int añoInicio) {
+        this.añoInicio = añoInicio;
+    }
+
+    public int getCantidadCursos() {
+        return cantidadCursos;
+    }
+
+    public void setCantidadCursos(int cantidadCursos) {
+        this.cantidadCursos = cantidadCursos;
+    }
 }

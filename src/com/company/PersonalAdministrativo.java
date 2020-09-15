59ae9eece7458915035e1de8a760f3444c1811e7
Sarita <atanacio.sarita@pucp.pe>
2020-09-15 08:38:19 -0500

    clases

---
+++ src/com/company/PersonalAdministrativo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class PersonalAdministrativo extends Usuario{
+
+    private String cargo;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}

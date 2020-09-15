ae8f17ee4d9678866a8fda64098dce75208077a3
Samuel Caro <a20170086@pucp.edu.pe>
2020-09-15 08:41:36 -0500

    Samuel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+
     }
+
 }
